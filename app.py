19e435ea564077e1cd84d5eeef29824a9f879592
Denver Judal <denver.judal@technomancer.biz>
2023-01-11 17:43:48 +0800

    added runtime flag and modified width and height parsing

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def update_board(an_array, a_new_array):
     if active_neighbors == 3:
       a_new_array[x, y] = 1
 
-def conway(VP_HEIGHT, VP_WIDTH, ITERATIONS, an_array):
+def conway(HEIGHT, WIDTH, ITERATIONS, an_array):
   time.sleep(0.5)
   dpg.delete_item("viewport_back", children_only=True)
 
@@ -28,20 +28,20 @@ def conway(VP_HEIGHT, VP_WIDTH, ITERATIONS, an_array):
       gpu_new_array = cuda.to_device(np.copy(an_array))
 
       threadsperblock = (32, 32) #fully utilize 1024 threads per block
-      blockspergrid_x = math.ceil((VP_WIDTH//16) / threadsperblock[0])
-      blockspergrid_y = math.ceil((VP_HEIGHT//16) / threadsperblock[1])
+      blockspergrid_x = math.ceil(WIDTH / threadsperblock[0])
+      blockspergrid_y = math.ceil(HEIGHT / threadsperblock[1])
       blockspergrid = (blockspergrid_x, blockspergrid_y)
 
       update_board[blockspergrid, threadsperblock](gpu_old_array, gpu_new_array)
       an_array = gpu_new_array.copy_to_host()
 
-      for i in range(0, VP_WIDTH, 16):
-        for j in range(0, VP_HEIGHT, 16):
-          value = an_array[i//16, j//16]
+      for i in range(WIDTH):
+        for j in range(HEIGHT):
+          value = an_array[i, j]
           if value == 0:
-            dpg.draw_rectangle((i, j), (i+16, j+16), fill = (255, 255, 255, 255), parent = "viewport_back")
+            dpg.draw_rectangle((i * PIXEL_DIM[0], j * PIXEL_DIM[1]), ((i * PIXEL_DIM[0]) + PIXEL_DIM[0], (j * PIXEL_DIM[1]) + PIXEL_DIM[1]), fill = (255, 255, 255, 255), parent = "viewport_back")
           else:
-            dpg.draw_rectangle((i, j), (i+16, j+16), fill = (0, 0, 0, 0), parent = "viewport_back")
+            dpg.draw_rectangle((i * PIXEL_DIM[0], j * PIXEL_DIM[1]), ((i * PIXEL_DIM[0]) + PIXEL_DIM[0], (j * PIXEL_DIM[1]) + PIXEL_DIM[1]), fill = (0, 0, 0, 0), parent = "viewport_back")
   
       dpg.render_dearpygui_frame()
       time.sleep(0.5)
@@ -50,34 +50,37 @@ def conway(VP_HEIGHT, VP_WIDTH, ITERATIONS, an_array):
     break
   dpg.destroy_context()
 
-def create_board(VP_HEIGHT, VP_WIDTH, ITERATIONS):
+def create_board(HEIGHT, WIDTH, ITERATIONS):
   dpg.setup_dearpygui()
   dpg.add_viewport_drawlist(front=False, tag="viewport_back")
   dpg.show_viewport()
 
-  an_array = np.asmatrix([[random.randint(0, 1) for _ in range((VP_HEIGHT)//16)] for _ in range((VP_WIDTH)//16)])
+  an_array = np.asmatrix([[random.randint(0, 1) for _ in range(HEIGHT)] for _ in range(WIDTH)])
 
-  for i in range(0, VP_WIDTH, 16):
-    for j in range(0, VP_HEIGHT, 16):
-      value = an_array[i//16, j//16]
+  for i in range(WIDTH):
+    for j in range(HEIGHT):
+      value = an_array[i, j]
       if value == 0:
-        dpg.draw_rectangle((i, j), (i+16, j+16), fill = (255, 255, 255, 255), parent = "viewport_back")
+        dpg.draw_rectangle((i * PIXEL_DIM[0], j * PIXEL_DIM[1]), ((i * PIXEL_DIM[0]) + PIXEL_DIM[0], (j * PIXEL_DIM[1]) + PIXEL_DIM[1]), fill = (255, 255, 255, 255), parent = "viewport_back")
       else:
-        dpg.draw_rectangle((i, j), (i+16, j+16), fill = (0, 0, 0, 0), parent = "viewport_back")
+        dpg.draw_rectangle((i * PIXEL_DIM[0], j * PIXEL_DIM[1]), ((i * PIXEL_DIM[0]) + PIXEL_DIM[0], (j * PIXEL_DIM[1]) + PIXEL_DIM[1]), fill = (0, 0, 0, 0), parent = "viewport_back")
   
   dpg.render_dearpygui_frame()
-  conway(VP_HEIGHT, VP_WIDTH, ITERATIONS, an_array)
+  conway(HEIGHT, WIDTH, ITERATIONS, an_array)
 
 parser = argparse.ArgumentParser(description = "Initialize board dimensions")
 parser.add_argument('--height', type = int, help = "specify height of the board, must be a multiple of 16", required = True)
 parser.add_argument('--width', type = int, help = "specify width of the board, must be a multiple of 16", required = True)
 parser.add_argument('--iterations', type = int, help = "number of iteration; must be a positive integer", required = True)
+parser.add_argument('--runtime', help = "measure runtime instead of presenting simulation", action='store_true')
 
 args = vars(parser.parse_args())
-VP_WIDTH = args["width"]
-VP_HEIGHT = args["height"]
+WIDTH = args["width"]
+HEIGHT = args["height"]
 ITERATIONS = args["iterations"]
+RUNTIME_FLAG = args["runtime"]
+PIXEL_DIM = (16, 16)
 
 dpg.create_context()
-dpg.create_viewport(title='Conway\'s Game of Life: GPU', width=VP_WIDTH, height=VP_HEIGHT, resizable = False)
-create_board(VP_HEIGHT, VP_WIDTH, ITERATIONS)
+dpg.create_viewport(title='Conway\'s Game of Life: GPU Naive', width=WIDTH*PIXEL_DIM[0], height=HEIGHT*PIXEL_DIM[1]+50, resizable = False)
+create_board(HEIGHT, WIDTH, ITERATIONS)
